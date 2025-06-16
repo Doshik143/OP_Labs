391c65127a86e033b7abb10e8b9393aaf198ae4d
Doshik143 <ipz232_ddo@student.ztu.edu.ua>
2025-06-16 21:46:45 +0300

    Lab7

---
+++ Lab7_OP/1/1.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	float x, y;
+	printf("\nВведіть x: ");
+	scanf_s("%f", &x);
+	printf("Введіть y: ");
+	scanf_s("%f", &y);
+	printf("\n---------------------\n\n");
+	if (y >= 0)
+	{
+		if (x >= 0)
+		{
+			if (y >= x + 1)
+			{
+				printf("True\n");
+			}
+			else { printf("False\n"); }
+		}
+		else
+		{
+			if (y >= 1 - x)
+			{
+				printf("True\n");
+			}
+			else { printf("False\n"); }
+		}
+	}
+	else if (y <= 0)
+	{
+		if (x <= 0)
+		{
+			if (y <= x + 1)
+			{
+				printf("True\n");
+			}
+			else { printf("False\n"); }
+		}
+		else
+		{
+			if (y <= 1 - x)
+			{
+				printf("True\n");
+			}
+			else { printf("False\n"); }
+		}
+	}
+	return 0;
+}
---
+++ Lab7_OP/2/2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int number;
+	printf("\nВведіть число: ");
+	scanf_s("%d", &number);
+	printf("\n---------------------\n\n");
+	(number % 3 == 0 && number % 7 == 0) ? printf("Число кратне 3-м і 7-ми\n") : printf("Число не є кратним 3-м і 7-ми\n");
+	return 0;
+}
